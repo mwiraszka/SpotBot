2ea0e46ec841dab3adb3bea9522481f88837d213
Michal Wiraszka <michal@wiraszka.com>
2020-10-15 22:05:00 -0400

    15.10.20 spotify client complete (following CodingEntrepreneurs Day 19 tutorial)

--- spotbot.py
+++ spotbot.py
@@ -7,16 +7,18 @@
 # 12.10.20 initial commit to github repo
 # 13.10.20 client credentials flow & initial Spotify Web API request
 # 14.10.20 client object rewritten as a SpotifyAPI class
+# 15.10.20 spotify client complete (following CodingEntrepreneurs Day 19 tutorial)
 
-import requests
+
 import datetime
-from urllib.parse import urlencode
 import base64
+from urllib.parse import urlencode
+import requests
 
 # ---Spotify Base API---
 class SpotifyAPI(object):
 	access_token = None
-	acess_token_expires = datetime.datetime.now()
+	access_token_expires = datetime.datetime.now()
 	access_token_did_expire = True
 	client_id = None
 	client_secret = None
@@ -55,7 +57,7 @@ class SpotifyAPI(object):
 		token_headers = self.get_token_headers()
 		r = requests.post(token_url, data=token_data, headers=token_headers)
 		if r.status_code not in range(200,299):
-			return False
+			raise Exception('Could not authenticate client.')
 		data = r.json()
 		now = datetime.datetime.now()
 		access_token = data['access_token']
@@ -66,27 +68,76 @@ class SpotifyAPI(object):
 		self.access_token_did_expire = expires < now
 		return True
 
+	def get_access_token(self):
+		token = self.access_token
+		expires = self.access_token_expires
+		now = datetime.datetime.now()
+		if expires < now or token == None:
+			self.perform_auth()
+			return self.get_access_token()
+		return token
+
+	def get_resource_headers(self):
+		access_token = self.get_access_token()
+		headers = {
+			'Authorization': f'Bearer {access_token}'
+		}
+		return headers
+
+	def get_resource(self, lookup_id, resource_type='albums', version='v1'):
+		endpoint = f'https://api.spotify.com/{version}/{resource_type}/{lookup_id}'
+		headers = self.get_resourse_headers()
+		r = requests.get(enpoint, headers=headers)
+		if r.status_code not in range(200, 299):
+			return {}
+		print(r.json())
+		return r.json()
+
+	def get_album(self, _id):
+		return self.get_resource(_id, resource_type='albums')
+
+	def get_artist(self, _id):
+		return self.get_resource(_id, resource_type='artists')
+
+
+	def base_search(self, query_params):
+		headers = self.get_resource_headers()
+		endpoint = 'https://api.spotify.com/v1/search'
+		lookup_url = f'{endpoint}?{query_params}'
+		r = requests.get(lookup_url, headers=headers)
+		if r.status_code not in range(200, 299):
+			return {}
+		print(r.json())
+		return r.json()
+
+	def search(self, query=None, operator=None, operator_query=None, search_type='artist'):
+		if query == None:
+			raise Exception('A query is required.')
+		if isinstance(query, dict):
+			# Converts dict to list
+			query = ' '.join([f'{k}:{v}' for k,v in query.items()])
+		if operator != None and operator_query != None:
+			if operator.lower() == 'or' or operator.lower() == 'not':
+				operator = operator.upper()
+				if isinstance(operator_query, str):
+					query = f'{query} {operator} {operator_query}'
+		# Converts a dictionary into a URL-ready string
+		query_params = urlencode({'q': query, 'type': search_type.lower()})
+		print(query_params)
+		return self.base_search(query_params)
+
+
+
 # ---Spotify client---	
 method = 'POST'
 client_id = 'f6d4f8e2d7934b1cbaac6f700261bcbf'
-client_secret = None #temporarily removed
+client_secret = #hidden
 
 spotify = SpotifyAPI(client_id, client_secret)
-spotify.perform_auth()
+spotify.search({'track': 'Make It Wit Chu'}, search_type='track')
 
 
-# ---Search query---
-headers = {
-	'Authorization': f'Bearer {spotify.access_token}'
-}
-endpoint = 'https://api.spotify.com/v1/search'
 
-# Converts a dictionary into a URL-ready string
-data = urlencode({'q': 'Time', 'type': 'track'})
-
-lookup_url = f'{endpoint}?{data}'
-r = requests.get(lookup_url, headers=headers)
-print(r.json())
 
 
 
