2571a8730b3b1e1c1261d1bec752a1e24a75b92e
Michal Wiraszka <michal@wiraszka.com>
2021-04-15 18:34:26 -0400

    tslint errors fixed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,36 +8,36 @@ import { SpotifyService } from '../spotify.service'
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  // error: any
-  // accessToken = ''
-  // refreshToken = ''
-  // queryObject: any
+  error: any
+  accessToken = ''
+  refreshToken = ''
+  queryObject: any
 
-  // username = ''
-  // email = ''
+  username = ''
+  email = ''
 
-  // constructor(private actRoute: ActivatedRoute, private spotifyService: SpotifyService) {
-  //   this.actRoute.queryParamMap.subscribe((params) => {
-  //     console.log(params)
-  //     this.queryObject = { ...params.keys, ...params }
-  //     this.error = this.queryObject.params.error
-  //     this.accessToken = this.queryObject.params.access_token
-  //     this.refreshToken = this.queryObject.params.refresh_token
-  //     console.log('Found access token=' + this.accessToken)
-  //     if (this.accessToken) {
-  //       this.loadUserDetails()
-  //     }
-  //   })
-  // }
+  constructor(private actRoute: ActivatedRoute, private spotifyService: SpotifyService) {
+    this.actRoute.queryParamMap.subscribe((params) => {
+      console.log(params)
+      this.queryObject = { ...params.keys, ...params }
+      this.error = this.queryObject.params.error
+      this.accessToken = this.queryObject.params.access_token
+      this.refreshToken = this.queryObject.params.refresh_token
+      console.log('Found access token=' + this.accessToken)
+      if (this.accessToken) {
+        this.loadUserDetails()
+      }
+    })
+  }
 
-  // loadUserDetails() {
-  //   this.spotifyService.getUserProfile(this.accessToken).subscribe((result) => {
-  //     console.log('Found result details=' + JSON.stringify(result))
-  //     if (result) {
-  //       const spotifyProfileData = JSON.parse(JSON.stringify(result))
-  //       this.username = spotifyProfileData.display_name
-  //       this.email = spotifyProfileData.email
-  //     }
-  //   })
-  // }
+  loadUserDetails() {
+    this.spotifyService.getUserProfile(this.accessToken).subscribe((result) => {
+      console.log('Found result details=' + JSON.stringify(result))
+      if (result) {
+        const spotifyProfileData = JSON.parse(JSON.stringify(result))
+        this.username = spotifyProfileData.display_name
+        this.email = spotifyProfileData.email
+      }
+    })
+  }
 }
